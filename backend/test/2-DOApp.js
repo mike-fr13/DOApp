6817763ed409e0531944aa280e8970b298950700
ytison <yannick.tison@gmail.com>
2023-07-10 15:16:57 +0200

    add some  Token Pair Managmeent tests

--- backend/test/2-DOApp.js
+++ backend/test/2-DOApp.js
@@ -1,36 +1,77 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers")
+const { expect } = require('chai')
+const { BigNumber } = require("ethers")
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
 
+const ADDRESS_0 = "0x0000000000000000000000000000000000000000"
+const MCKA_NAME = "Mock Token A"
+const MCKA_SYMBOL = "MCKA"
+const MCKB_NAME = "Mock Token B"
+const MCKB_SYMBOL = "MCKB"
+const INITIAL_SUPPLY = 1000
 
 describe("DOApp", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
+
   async function deployDOAppFixture() {
-    // Contracts are deployed using the first signer/account by default
     const [owner, account1, account2, account3, account4] = await ethers.getSigners();
     const DOApp = await ethers.getContractFactory("DOApp");
     const dOApp = await DOApp.deploy();
 
     const TokenA = await ethers.getContractFactory("MockERC20");
-    const tokenA = await TokenA.deploy("Mock Token A", "MCKA", 1000);
+    const tokenA = await TokenA.deploy(MCKA_NAME,MCKA_SYMBOL,INITIAL_SUPPLY);
 
     const TokenB = await ethers.getContractFactory("MockERC20");
-    const tokenB = await TokenB.deploy("Mock Token B", "MCKB",1000);
+    const tokenB = await TokenB.deploy(MCKB_NAME,MCKB_SYMBOL,INITIAL_SUPPLY);
+
+    const MockChainLinkAggregatorV3 = await ethers.getContractFactory("MockChainLinkAggregatorV3");
+    const mockChainLinkAggregatorV3 = await MockChainLinkAggregatorV3.deploy(ADDRESS_0,true);
 
 
-    return { dOApp, tokenA, tokenB, owner, account1, account2, account3, account4 };
+    return { dOApp, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4 };
   }
 
   describe("Deployment", function () {
     it("Should set the right owner", async function () {
-      const { dOApp, owner } = await loadFixture(deployDOAppFixture);
+      const { dOApp, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4} 
+        = await loadFixture(deployDOAppFixture);
       expect(await dOApp.owner()).to.equal(owner.address);
     });
+  });
+
+
+  describe("Token Pair Management", function () {
+    it("Should add a token Pair", async function () {
+      const {dOApp, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4} = await loadFixture(deployDOAppFixture);
+      //console.log("tokenA : ", tokenA.address);
+      //console.log("tokenB : ", tokenB.address);
+      await expect(dOApp.addTokenPair(tokenA.address, tokenB.address, mockChainLinkAggregatorV3.address))
+                    .to.emit(dOApp,"TokenPAirAdded").withArgs(anyValue, tokenA.address, tokenB.address, mockChainLinkAggregatorV3.address)
+
+      let eventFilter = dOApp.filters.TokenPAirAdded()
+      let events = await dOApp.queryFilter(eventFilter, "latest")
+      let hashOfPair = events[0].args[0]
+      //console.log("hashPair : ",hashOfPair)
+      let tokenPair = await dOApp.tokenPairs(hashOfPair)
+      //console.log(tokenPair)
+      expect (tokenPair.TokenAddressA).to.equal(tokenA.address)
+      expect (tokenPair.TokenAddressB).to.equal(tokenB.address)
+      expect (tokenPair.ChainlinkPriceFetcher).to.equal(mockChainLinkAggregatorV3.address)
+      expect (tokenPair.enabled).to.be.false
+    });
+
+    it("Should revert when trying to add a pair with a null tokenA address", async function () {
+      const {dOApp, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4} = await loadFixture(deployDOAppFixture);
+      await expect(dOApp.addTokenPair(ADDRESS_0, tokenB.address, mockChainLinkAggregatorV3.address))
+                    .to.be.revertedWith("tokenA address must be defined")
+    });
+
+    it("Should revert when trying to add a pair with a null tokenB address", async function () {
+      const {dOApp, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4} = await loadFixture(deployDOAppFixture);
+      await expect(dOApp.addTokenPair(tokenA.address,ADDRESS_0, mockChainLinkAggregatorV3.address))
+                    .to.be.revertedWith("tokenB address must be defined")
+    });
+
+
   });
 
   /*
