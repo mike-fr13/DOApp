3ed50f1e9eb1ee4f3e3ec01f8a5a788983dae0ad
ytison <yannick.tison@gmail.com>
2023-07-15 23:40:40 +0200

    add comments

--- backend/temp_contracts/AAVESupply.sol
+++ backend/temp_contracts/AAVESupply.sol
@@ -4,7 +4,25 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
 import "@aave/core-v3/contracts/interfaces/IPool.sol";
 
-contract AAVESupply {
+
+/**
+ * @author  Yannick Tison
+ * @title   AAVE v3 IPool test : supply / withdraw ...
+ * @dev     1- call depositDAIToContract
+ * @dev     /!\ First you must externally call approve on the DAI contract, to get allowance for this contract to transferFrom DAI from your account
+ * @dev     2 - call approveLendingPool to get allowance from LendigPool to transferFrom DAI from this contract
+ * @dev     3 - call supplyForContract or SupplyFromMessenger 
+ * @dev         => supplyForContract : corresponding aPolDAI will be transfer to this contract (the contract own the supply)
+ * @dev         => SupplyFromMsgSenger : corresponding aPolDAI will be transfer to this msg.sender (Msg.sender own the supply)
+ * @dev     4 - call withdrawFromAAVEToContract or withdrawFromAAVEToMsgSender
+ * @dev         => withdrawFromAAVEToContract : redeem aPolDAI from currrent contract and get back DAI to current contract
+ * @dev         => withdrawFromAAVEToMsgSender : redeem aPolDAI from currrent contract and get back DAI to msg.sender
+ * @notice  just test it ! 
+ * @notice  hardcoded token (DAI) and IPoolAddressProvider are mumbai configuration
+ * @notice  see : https://docs.aave.com/developers/deployed-contracts/v3-testnet-addresses#polygon-mumbai  
+ */
+
+contract AAVEv3IPoolTest {
 
     IERC20 DAI = IERC20(0xF14f9596430931E177469715c591513308244e8F);
 
@@ -52,16 +70,29 @@ contract AAVESupply {
         return DAI.allowance(address(this),address(lendingPool));
     }
 
-    /*
-    1- call depositDAIToContract
-        /!\ First you must externally call approve on the DAI contract, to get allowance for this contract to transferFrom DAI from your account
-    2 - call approveLendingPool to get allowance from LendigPool to transferFrom DAI from this contract
-    3 - call supplyForContract or SupplyFromMessenger 
-        => supplyForContract : corresponding aPolDAI will be transfer to this contract (the contract own the supply)
-        => SupplyFromMsgSenger : corresponding aPolDAI will be transfer to this msg.sender (Msg.sender own the supply)
-    4 - call withdrawFromAAVEToContract or withdrawFromAAVEToMsgSender
-        => withdrawFromAAVEToContract : redeem aPolDAI from currrent contract and get back DAI to current contract
-        => withdrawFromAAVEToMsgSender : redeem aPolDAI from currrent contract and get back DAI to msg.sender
-    */
+    // get ReserveData for specified asset 
+    // see https://docs.aave.com/developers/core-contracts/pool#getreservedata
+    // https://github.com/aave/aave-v3-core/blob/27a6d5c83560694210849d4abf09a09dec8da388/contracts/interfaces/IPool.sol#L582
+    function getReserveData(address _asset) external view returns(DataTypes.ReserveData memory) {
+        return IPool(lendingPool).getReserveData(_asset);
+    }
+
+    function getAtokenAddress(address _asset) external view returns(address aTokenAddress) {
+        return IPool(lendingPool).getReserveData(_asset).aTokenAddress;
+    }
+
+    // see 
+    // https://github.com/aave/aave-v3-core/blob/27a6d5c83560694210849d4abf09a09dec8da388/contracts/interfaces/IPool.sol#L477
+    // https://docs.aave.com/developers/core-contracts/pool#getuseraccountdata
+    function getUserAccountData(address _user) external view returns (
+      uint256 totalCollateralBase,
+      uint256 totalDebtBase,
+      uint256 availableBorrowsBase,
+      uint256 currentLiquidationThreshold,
+      uint256 ltv,
+      uint256 healthFactor
+    ) {
+        return (IPool(lendingPool).getUserAccountData(_user));
+    }
 
 }
