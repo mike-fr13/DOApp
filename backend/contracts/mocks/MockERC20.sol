a96303a5d7c34f2223fba839450c999391fc87a2
ytison <yannick.tison@gmail.com>
2023-07-17 02:34:26 +0200

    inherit ECR20Burnable

--- backend/contracts/mocks/MockERC20.sol
+++ backend/contracts/mocks/MockERC20.sol
@@ -3,14 +3,15 @@
 pragma solidity >=0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MockERC20 is ERC20 {
+contract MockERC20 is ERC20, ERC20Burnable {
 
     constructor(string memory _name, string memory _symbol, uint _initialSupply) ERC20(_name, _symbol){
         _mint(msg.sender, _initialSupply);
     }
 
-    function mint(address _receiver, uint _amount) external {
+    function mint(address _receiver, uint _amount) public {
         _mint(_receiver, _amount);
     }
 
