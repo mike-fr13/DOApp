c89dcf39de2f75156102d370e89cf8428f5e645f
ytison <yannick.tison@gmail.com>
2023-07-19 01:11:59 +0200

    WIP : script init

--- backend/test/lib/Fixtures.js
+++ backend/test/lib/Fixtures.js
@@ -3,7 +3,7 @@ const Constant = require("./Constants.js")
 const isLogEnable = false;
 
 async function deployDOApp_Fixture() {
-  const [owner, account1, account2, account3, account4] = await ethers.getSigners();
+  const [owner, account1, account2, account3, account4, account5, account6] = await ethers.getSigners();
 
   // deploy DOApp main contract
   const DataStorage = await ethers.getContractFactory('DataStorage');
@@ -48,13 +48,13 @@ async function deployDOApp_Fixture() {
   const mockUniswapISwapRouter = await MockUniswapISwapRouter.deploy();
   isLogEnable ? console.log(`mockUniswapISwapRouter deployed to ${mockUniswapISwapRouter.address}`):{}
 
-  return { doApp, dataStorage,tokenA, tokenB, mockChainLinkAggregatorV3, mockAAVEPoolAddressesProvider, mockAavePool, mockUniswapISwapRouter, owner, account1, account2, account3, account4 };
+  return { doApp, dataStorage,tokenA, tokenB, mockChainLinkAggregatorV3, mockAAVEPoolAddressesProvider, mockAavePool, mockUniswapISwapRouter, owner, account1, account2, account3, account4, account5, account6 };
 }
 
 // deploy contracts and add a token Pair
 async function deploy_AddATokenPair_Fixture() {
   //deploy contracts
-  const { doApp, dataStorage,tokenA, tokenB, mockChainLinkAggregatorV3, mockAAVEPoolAddressesProvider, mockUniswapISwapRouter, owner, account1, account2, account3, account4 }
+  const { doApp, dataStorage,tokenA, tokenB, mockChainLinkAggregatorV3, mockAAVEPoolAddressesProvider, mockUniswapISwapRouter, owner, account1, account2, account3, account4, account5, account6}
     = await loadFixture(deployDOApp_Fixture);
 
   //add a token pair
@@ -71,12 +71,12 @@ async function deploy_AddATokenPair_Fixture() {
   let events = await dataStorage.queryFilter(eventFilter, 'latest')
   let pairId = events[0].args[0]
 
-  return { doApp, dataStorage,tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, pairId };
+  return { doApp, dataStorage,tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, account5, account6, pairId };
 }
 
 async function deploy_AddATokenPair_MinToken_Fixture() {
   // deploy contract, add a token pair and mint TokenA and TokenB for account1 to account3
-  const { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, pairId }
+  const { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, account5, account6, pairId }
     = await loadFixture(deploy_AddATokenPair_Fixture);
 
   //mint tokenA
@@ -89,7 +89,7 @@ async function deploy_AddATokenPair_MinToken_Fixture() {
   await tokenB.mint(account2.address, Constant.TOKEN_INITIAL_SUPPLY)
   await tokenB.mint(account3.address, Constant.TOKEN_INITIAL_SUPPLY)
 
-  return { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, pairId };
+  return { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, account5, account6, pairId };
 }
 
 // deploy contract, add a token pair, mint TokenA and TokenB, deposit tokenA and token B in the pair for account1 to account3
@@ -99,7 +99,7 @@ async function deploy_AddATokenPair_MinToken_Fixture() {
 
 async function deploy_AddATokenPair_MinToken_DepositToken_Fixture() {
   //deploy contracts
-  const { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, pairId }
+  const { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, account5, account6, pairId }
     = await loadFixture(deploy_AddATokenPair_MinToken_Fixture);
 
   //acount1 => deposit token A and token B
@@ -116,13 +116,13 @@ async function deploy_AddATokenPair_MinToken_DepositToken_Fixture() {
   await tokenB.connect(account3).approve(doApp.address, Constant.TOKENB_DEPOSIT_AMOUNT)
   await doApp.connect(account3).depositTokenB(pairId, Constant.TOKENB_DEPOSIT_AMOUNT)
 
-  return { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, pairId };
+  return { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, account5, account6, pairId };
 }
 
 
 async function deploy_Prepare_One_DCA_Config_Fixture() {
   //deploy contracts
-  const { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, pairId }
+  const { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, account5, account6, pairId }
     = await loadFixture(deploy_AddATokenPair_MinToken_DepositToken_Fixture);
 
   await dataStorage.connect(account1).addDCAConfig(
@@ -134,13 +134,13 @@ async function deploy_Prepare_One_DCA_Config_Fixture() {
     Constant.DCA_CONFIG_1_SCALING_FACTOR,
     Constant.DCA_CONFIG_1_DELAY
   )
-  return { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, pairId };
+  return { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, account5, account6, pairId };
 }
 
 
 async function deploy_Prepare_All_DCA_Config_Fixture() {
   //deploy contracts
-  const {doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, pairId }
+  const {doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, account5, account6, pairId }
     = await loadFixture(deploy_AddATokenPair_MinToken_DepositToken_Fixture);
 
   await dataStorage.connect(account1).addDCAConfig(
@@ -184,7 +184,68 @@ async function deploy_Prepare_All_DCA_Config_Fixture() {
     Constant.DCA_CONFIG_4_DELAY
   )
 
-  return { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, pairId };
+  await dataStorage.connect(account2).addDCAConfig(
+    pairId,
+    Constant.DCA_CONFIG_5_IS_SWAP_TOKEN_A_FOR_TOKEN_B,
+    Constant.DCA_CONFIG_5_MIN,
+    Constant.DCA_CONFIG_5_MAX,
+    Constant.DCA_CONFIG_5_AMOUNT,
+    Constant.DCA_CONFIG_5_SCALING_FACTOR,
+    Constant.DCA_CONFIG_5_DELAY
+  )
+
+  await dataStorage.connect(account3).addDCAConfig(
+    pairId,
+    Constant.DCA_CONFIG_6_IS_SWAP_TOKEN_A_FOR_TOKEN_B,
+    Constant.DCA_CONFIG_6_MIN,
+    Constant.DCA_CONFIG_6_MAX,
+    Constant.DCA_CONFIG_6_AMOUNT,
+    Constant.DCA_CONFIG_6_SCALING_FACTOR,
+    Constant.DCA_CONFIG_6_DELAY
+  )
+
+  await dataStorage.connect(account4).addDCAConfig(
+    pairId,
+    Constant.DCA_CONFIG_7_IS_SWAP_TOKEN_A_FOR_TOKEN_B,
+    Constant.DCA_CONFIG_7_MIN,
+    Constant.DCA_CONFIG_7_MAX,
+    Constant.DCA_CONFIG_7_AMOUNT,
+    Constant.DCA_CONFIG_7_SCALING_FACTOR,
+    Constant.DCA_CONFIG_7_DELAY
+  )
+
+  await dataStorage.connect(account2).addDCAConfig(
+    pairId,
+    Constant.DCA_CONFIG_8_IS_SWAP_TOKEN_A_FOR_TOKEN_B,
+    Constant.DCA_CONFIG_8_MIN,
+    Constant.DCA_CONFIG_8_MAX,
+    Constant.DCA_CONFIG_8_AMOUNT,
+    Constant.DCA_CONFIG_8_SCALING_FACTOR,
+    Constant.DCA_CONFIG_8_DELAY
+  )
+
+  await dataStorage.connect(account5).addDCAConfig(
+    pairId,
+    Constant.DCA_CONFIG_9_IS_SWAP_TOKEN_A_FOR_TOKEN_B,
+    Constant.DCA_CONFIG_9_MIN,
+    Constant.DCA_CONFIG_9_MAX,
+    Constant.DCA_CONFIG_9_AMOUNT,
+    Constant.DCA_CONFIG_9_SCALING_FACTOR,
+    Constant.DCA_CONFIG_9_DELAY
+  )
+
+  await dataStorage.connect(account6).addDCAConfig(
+    pairId,
+    Constant.DCA_CONFIG_10_IS_SWAP_TOKEN_A_FOR_TOKEN_B,
+    Constant.DCA_CONFIG_10_MIN,
+    Constant.DCA_CONFIG_10_MAX,
+    Constant.DCA_CONFIG_10_AMOUNT,
+    Constant.DCA_CONFIG_10_SCALING_FACTOR,
+    Constant.DCA_CONFIG_10_DELAY
+  )
+
+
+  return { doApp, dataStorage, tokenA, tokenB, mockChainLinkAggregatorV3, owner, account1, account2, account3, account4, account5, account6, pairId };
 }
 
 
