b6dc206d7f81a1626a636ffae64abc491971d55b
ytison <yannick.tison@gmail.com>
2023-07-23 00:26:26 +0200

    set 0.8.19 for sol (mumbai lmit)

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -38,7 +38,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.20",
+        version: "0.8.19",
         settings: {
           optimizer: {
             enabled: true,
